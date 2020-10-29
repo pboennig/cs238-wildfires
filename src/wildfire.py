f3f6f5c778d97e786a7f4c3077ebc17e845fcffc
pboennig <pboennig@gmail.com>
2020-10-28 18:10:24 -1000

    add actions

--- src/wildfire.py
+++ src/wildfire.py
@@ -25,6 +25,7 @@ class FireGrid:
         S = []
         self.n = n
         self.reward = 0
+        self.resource_cost = 0
         for i in range(n):
             r = []
             for j in range(n):
@@ -39,11 +40,9 @@ class FireGrid:
         self.S = S
 
     """
-    Given a grid representing our state, print a representation of the fires
-    currently burning. An 'X' represents a burning fire, while a '_' represents
-    a fire-free region.
+    Helper functions to display the fires, fuel, and resources.
     """
-    def show_fire_status(self):
+    def show_fires(self):
         symbol = {True: 'X', False: '_'}
         for row in self.S:
             print(' '.join([symbol[cell.fire] for cell in row]))
@@ -52,6 +51,14 @@ class FireGrid:
         for row in self.S:
             print(' '.join([str(round(cell.fuel, 2)) for cell in row]))
 
+    def show_resources(self):
+        symbol = {True: '0', False: '_'}
+        for row in self.S:
+            print(' '.join([symbol[cell.resources] for cell in row]))
+
+    """
+    Helper function to calculate the number of neighboring cells on fire.
+    """ 
     def neighbors_on_fire(self, i, j):
         num_neighbors = 0
         if i - 1 >= 0:
@@ -64,13 +71,20 @@ class FireGrid:
             num_neighbors += self.S[i][j+1].fire
         return num_neighbors
 
+    """
+    Transition function that changes the state of the grid based on existing fire conditions,
+    the dryness of the cell, and our resources. Also updates our reward to include the cost
+    of burning fires and using resources.
+    """
     def transition(self):
         S_prime = self.S
         for i in range(self.n):
             for j in range(self.n):
                 if self.S[i][j].fire:
+                    # for now, cannot put out fires but only contain/prevent them
                     percentage_burned = .2 * np.random.random_sample()
                     if S_prime[i][j].fuel < percentage_burned:
+                        # we're out of fuel
                         S_prime[i][j].fuel = 0
                         S_prime[i][j].fire = False
                     else:
@@ -80,15 +94,25 @@ class FireGrid:
                     S_prime[i][j].property -= property_lost 
                 else:
                     threshold = self.neighbors_on_fire(i, j) / 4 * self.S[i][j].dryness * self.S[i][j].fuel
+                    threshold -= .5 * np.random.random_sample() * self.S[i][j].resources # if using resources may be able to reduce fire odds by 50%
+                    if self.S[i][j].resources:
+                        print(threshold)
                     S_prime[i][j].fire = np.random.random_sample() < threshold
         self.S = S_prime
+        self.reward -= self.resource_cost
 
+    """
+    Actions to remove or add resources to a specific cell.
+    """
+    def place_resource(self, i, j):
+        if not self.S[i][j].resources:
+            self.S[i][j].resources = True
+            self.resource_cost += 10
+        self.transition()
 
-grid = FireGrid(3)
-for i in range(20):
-    print("Iteration: {}".format(i))
-    print("-"*15)
-    grid.show_fire_status()
-    print(grid.reward)
-    print(" ")
-    grid.transition()
+    def remove_resource(self, i, j):
+        if self.S[i][j].resources:
+            self.S[i][j].resources = False
+            self.resource_cost -= 10
+        self.transition()
+
